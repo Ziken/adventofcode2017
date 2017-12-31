0c9b1184737439bfeabbea053c4861cc21fa53af
Ziken <patrykkazmierczak97@gmail.com>
2017-12-31 15:39:10 +0100

    Solution day 9

---
+++ day9/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+const INPUT_FILE = "input.txt"
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+func getInput() string {
+	input, err := ioutil.ReadFile(INPUT_FILE)
+	check(err)
+	return string(input)
+}
+func answerPart1And2(streamOfCharacters string) {
+	isGarbageOpen := false
+	ignoreSign := false
+	levelNesting := 0
+	score := 0
+	ignoredSignsAmount := 0
+	for _, s := range streamOfCharacters {
+		sign := string(s)
+		switch {
+			case ignoreSign:
+				ignoreSign = false
+			case sign == ">":
+				isGarbageOpen = false
+			case sign == "!" && isGarbageOpen:
+				ignoreSign = true
+			case isGarbageOpen:
+				ignoredSignsAmount++
+			case sign == "<":
+				isGarbageOpen = true
+			case sign == "{":
+				levelNesting++
+				score += levelNesting
+			case sign == "}":
+				levelNesting--
+		}
+	}
+	fmt.Println("Answer part 1:", score)
+	fmt.Println("Answer part 2:", ignoredSignsAmount)
+
+}
+func main() {
+	in := getInput()
+	answerPart1And2(in)
+}
